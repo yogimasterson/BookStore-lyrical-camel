48e94b94674d3747bb1adc5c3013441dc126b719
Michael Masterson <michaelmasterson@Michaels-MacBook-Pro.local>
2017-04-20 17:02:33 -0700

    changed from handlebars to pug

--- bookstore/server/database.js
+++ bookstore/server/database.js
@@ -1,8 +1,41 @@
+'use strict';
+
 const pg = require('pg');
 const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/michaelmasterson';
 
-const client = new pg.Client(connectionString);
-client.connect();
-const query = client.query(
-  'CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
-query.on('end', () => { client.end(); });
+const getArtworkById = function(artworkId){
+  return db.one("select * from artwork where artwor.id=$1", [artworkId])
+}
+
+const getAllArtworks = function(id){
+  return db.any("select * from artwork");
+}
+
+const getAllGenres = function(id){
+  return db.any("select * from genre");
+}
+
+const getAllArtists = function (id){
+  return db.any("select * from artist")
+}
+
+const getAllArtworksWithArtists = function() {
+  return getAllArtworks().then(function(artworks){
+    console.log('GOT ARTWORK', artworks)
+    const artworkIds = artworks.map(artwork => artwork.id)
+    const sql = `
+      SELECT
+        artists.*,
+        artwork.id
+      FROM
+        artists
+      JOIN
+        artwork
+      ON
+        artist.id = artwork.artist_id
+      WHERE
+        artwork.id IN ($1:csv)
+    `;
+    console.log('artworkIds', artwork)
+  })
+}
